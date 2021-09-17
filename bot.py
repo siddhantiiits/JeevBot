21691403b683895ef2141a1c94fa2e93103e4b61
Pulkit Goyal <54880505+pulkit0795@users.noreply.github.com>
2021-09-18 01:58:07 +0530

    Probably multiple users can use now

--- bot.py
+++ bot.py
@@ -4,40 +4,45 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 
 
-
-
 app = Flask(__name__)
 #
 l=[]
+Dict={}
 @app.route('/bot', methods=['POST'])
 def bot():
 
     incoming_msg = request.values.get('Body','').lower()
+    phone_num = request.values.get('From','').lower()
+    if phone_num in Dict:
+        print("Already in dict")
+    else:
+        Dict[phone_num]=[]
+
     resp = MessagingResponse()
 
     msg = resp.message()
 
     if incoming_msg in ('home' , 'hi'):
-        l.clear()
-
-
-    if not l and incoming_msg:
+        Dict[phone_num].clear()
         r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
 
-    if len(l) == 1 :
+
+    # if not Dict[phone_num] and incoming_msg:
+    #     r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
+
+    if len(Dict[phone_num]) == 1 :
         if incoming_msg == '1':
-            r = 'What kind of help do you want ?\n1) General Help \n2) Disease Related Help '
+            r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
         elif incoming_msg =='2':
             r = 'Please Enter your pincode '
-    if len(l)==2:
+    elif len(Dict[phone_num])==2:
         print(l)
-        if l[1] =='1':
+        if Dict[phone_num][1] =='1':
             if incoming_msg == '1':
                 r = 'Help is on the way'
-
             elif incoming_msg =='2':
                 r = "Disease help is on the way"
-        if l[1] == '2':
+        elif Dict[phone_num][1] == '2':
             r = 'Thanks we have recieved your pincode:' + incoming_msg
 
 
@@ -46,7 +51,7 @@ def bot():
     msg.body(r)
     # msg.media('https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
     if incoming_msg:
-        l.append(incoming_msg)
+        Dict[phone_num].append(incoming_msg)
 
     return str(resp)
 
