01311d9360dadf403f4e0e196cc5116ec5ef6b49
siddhantiiits <54790933+siddhantiiits@users.noreply.github.com>
2021-09-19 12:56:46 +0530

    conversation tree test

--- bot.py
+++ bot.py
@@ -25,51 +25,41 @@ def bot():
 
     msg = resp.message()
 
-    if incoming_msg in ('home','hi'):
-        stream = conversation
-        Dict[phone_num] = stream
-    else:
-        if len(Dict[phone_num]) >= incoming_msg:
-            stream = 'Give a valid input please! 🥺'
-        else:
-            stream = Dict[phone_num][incoming_msg]
-            Dict[phone_num] = stream
 
 
-    # if incoming_msg in ('home' , 'hi'):
-    #     Dict[phone_num].clear()
+
+    if incoming_msg in ('home' , 'hi'):
+        Dict[phone_num].clear()
+        r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
+
+
+    # if not Dict[phone_num] and incoming_msg:
     #     r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
-    #
-    #
-    # # if not Dict[phone_num] and incoming_msg:
-    # #     r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
-    #
-    # elif len(Dict[phone_num]) == 1 :
-    #     if incoming_msg == '1':
-    #         r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
-    #     elif incoming_msg =='2':
-    #         r = 'Please Enter your pincode '
-    # elif len(Dict[phone_num])==2:
-    #     print(l)
-    #     if Dict[phone_num][1] =='1' and incoming_msg=='1':
-    #         r = 'Help is on the way'
-    #     elif Dict[phone_num][1] =='1' and incoming_msg=='2':
-    #         r = "Disease help is on the way"
-    #     elif Dict[phone_num][1] == '2':
-    #         r = 'Thanks we have recieved your pincode:' + incoming_msg
-    # else:
-    #     r = 'Give a valid input please! 🥺'
-    #
-    #
-    #
-    #
-    # msg.body(r)
-    # # msg.media('https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
-    # if incoming_msg:
-    #     Dict[phone_num].append(incoming_msg)
-    msg.body(stream[0])
-    # if incoming_msg:
-    #     Dict[phone_num] = stream
+
+    elif len(Dict[phone_num]) == 1 :
+        if incoming_msg == '1':
+            r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
+        elif incoming_msg =='2':
+            r = 'Please Enter your pincode '
+    elif len(Dict[phone_num])==2:
+        print(l)
+        if Dict[phone_num][1] =='1' and incoming_msg=='1':
+            r = 'Help is on the way'
+        elif Dict[phone_num][1] =='1' and incoming_msg=='2':
+            r = "Disease help is on the way"
+        elif Dict[phone_num][1] == '2':
+            r = 'Thanks we have recieved your pincode:' + incoming_msg
+    else:
+        r = 'Give a valid input please! 🥺'
+
+
+
+
+    msg.body(r)
+    # msg.media('https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
+    if incoming_msg:
+        Dict[phone_num].append(incoming_msg)
+
 
     return str(resp)
 
