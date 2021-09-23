fa5b6e1c93b9c72c095285a63d70a34b4fa1411e
siddhantiiits <54790933+siddhantiiits@users.noreply.github.com>
2021-09-23 12:08:46 +0530

    Un-nested conditional flow

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 import pickle
+from flow import process
 
 
 
@@ -30,30 +31,32 @@ def bot():
 
     msg = resp.message()
 
-    if incoming_msg in ('home' , 'hi'):
-        # Dict[phone_num].clear()
-        Dict[phone_num]=[]
-        r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
+    r = process(Dict[phone_num],incoming_msg)
 
-
-    # if not Dict[phone_num] and incoming_msg:
+    # if incoming_msg in ('home' , 'hi'):
+    #     # Dict[phone_num].clear()
+    #     Dict[phone_num]=[]
     #     r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
-
-    elif len(Dict[phone_num]) == 1 :
-        if incoming_msg == '1':
-            r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
-        elif incoming_msg =='2':
-            r = 'Please Enter your pincode '
-    elif len(Dict[phone_num])==2:
-        # print(l)
-        if Dict[phone_num][1] =='1' and incoming_msg=='1':
-            r = 'Help is on the way'
-        elif Dict[phone_num][1] =='1' and incoming_msg=='2':
-            r = "Disease help is on the way"
-        elif Dict[phone_num][1] == '2':
-            r = 'Thanks we have recieved your pincode:' + incoming_msg
-    else:
-        r = 'Give a valid input please! 🥺'
+    #
+    #
+    # # if not Dict[phone_num] and incoming_msg:
+    # #     r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
+    #
+    # elif len(Dict[phone_num]) == 1 :
+    #     if incoming_msg == '1':
+    #         r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
+    #     elif incoming_msg =='2':
+    #         r = 'Please Enter your pincode '
+    # elif len(Dict[phone_num])==2:
+    #     # print(l)
+    #     if Dict[phone_num][1] =='1' and incoming_msg=='1':
+    #         r = 'Help is on the way'
+    #     elif Dict[phone_num][1] =='1' and incoming_msg=='2':
+    #         r = "Disease help is on the way"
+    #     elif Dict[phone_num][1] == '2':
+    #         r = 'Thanks we have recieved your pincode:' + incoming_msg
+    # else:
+    #     r = 'Give a valid input please! 🥺'
 
 
 
@@ -61,8 +64,7 @@ def bot():
     msg.body(r)
 
     # msg.media('https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
-    print("Value before appending")
-    print(Dict)
+
     if incoming_msg:
         Dict[phone_num].append(incoming_msg)
         with open('file.pkl', 'wb') as file:
@@ -70,11 +72,7 @@ def bot():
             pickle.dump(Dict, file)
             print("Dumped")
 
-    print("Value after appending")
-    print('--------\n\nDict is ')
-    print(Dict)
-    print('\n\n--------\nDict value for phone num ')
-    print(Dict[phone_num])
+
 
     return str(resp)
 
---
+++ flow.py
@@ -0,0 +1,25 @@
+# l is the list corresponding to Dict[phone_num]
+# incomingMsg is the incoming_msg
+def process(l,incomingMsg):
+    l.append(incomingMsg)
+
+    if len(l) == 1:
+        r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
+    elif l[1:] == [1]:
+        r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
+    elif l[1:] == [2]:
+        r = 'Please Enter your pincode '
+    elif l[1:] == [1,1]:
+        r = 'Help is on the way'
+    elif l[1:] == [1,2]:
+        r = "Disease help is on the way"
+    elif l[1] == 2 and l[2]:
+        r = 'Thanks we have recieved your pincode: ' + l[2]
+    else:
+        r = 'Give a valid input please! 🥺'
+        l.pop()
+
+    return r
+
+
+
