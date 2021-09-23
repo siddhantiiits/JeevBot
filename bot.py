bdd3f3ff1a45dd2f4a10eb27595d3b1733bd9345
siddhantiiits <54790933+siddhantiiits@users.noreply.github.com>
2021-09-23 12:32:14 +0530

    Structured

--- bot.py
+++ bot.py
@@ -28,49 +28,22 @@ def bot():
         Dict[phone_num]=[]
 
     resp = MessagingResponse()
-
     msg = resp.message()
 
+
+    # print(Dict[phone_num])
     r = process(Dict[phone_num],incoming_msg)
-
-    # if incoming_msg in ('home' , 'hi'):
-    #     # Dict[phone_num].clear()
-    #     Dict[phone_num]=[]
-    #     r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
-    #
-    #
-    # # if not Dict[phone_num] and incoming_msg:
-    # #     r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
-    #
-    # elif len(Dict[phone_num]) == 1 :
-    #     if incoming_msg == '1':
-    #         r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
-    #     elif incoming_msg =='2':
-    #         r = 'Please Enter your pincode '
-    # elif len(Dict[phone_num])==2:
-    #     # print(l)
-    #     if Dict[phone_num][1] =='1' and incoming_msg=='1':
-    #         r = 'Help is on the way'
-    #     elif Dict[phone_num][1] =='1' and incoming_msg=='2':
-    #         r = "Disease help is on the way"
-    #     elif Dict[phone_num][1] == '2':
-    #         r = 'Thanks we have recieved your pincode:' + incoming_msg
-    # else:
-    #     r = 'Give a valid input please! 🥺'
-
-
-
+    # print(Dict[phone_num])
 
     msg.body(r)
 
     # msg.media('https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
 
-    if incoming_msg:
-        Dict[phone_num].append(incoming_msg)
-        with open('file.pkl', 'wb') as file:
-            # A new file will be created
-            pickle.dump(Dict, file)
-            print("Dumped")
+
+    with open('file.pkl', 'wb') as file:
+        # A new file will be created
+        pickle.dump(Dict, file)
+        print("Dumped")
 
 
 
--- flow.py
+++ flow.py
@@ -1,20 +1,32 @@
 # l is the list corresponding to Dict[phone_num]
 # incomingMsg is the incoming_msg
 def process(l,incomingMsg):
-    print(l)
+    # print(l)
     l.append(incomingMsg)
+    # print(l)
+
+    if incomingMsg == 'home':
+        l.clear()
+        l.append('hi')
+    elif len(l) > 2 and incomingMsg == 'back':
+        l.pop()
+        l.pop()
+    elif len(l) == 2 and incomingMsg == 'back':
+        l.pop()
+
+
 
     if len(l) == 1:
         r = 'Hey! How can I help you today ? \n1) Help \n2) Vet Call'
-    elif l[1:] == [1]:
+    elif l[1:] == ['1']:
         r = 'What kind of help do you want ?\n*1)* General Help \n*2)* Disease Related Help '
-    elif l[1:] == [2]:
+    elif l[1:] == ['2']:
         r = 'Please Enter your pincode '
-    elif l[1:] == [1,1]:
+    elif l[1:] == ['1','1']:
         r = 'Help is on the way'
-    elif l[1:] == [1,2]:
+    elif l[1:] == ['1','2']:
         r = "Disease help is on the way"
-    elif l[1] == 2 and l[2]:
+    elif l[1] == '2' and l[2]:
         r = 'Thanks we have recieved your pincode: ' + l[2]
     else:
         r = 'Give a valid input please! 🥺'
