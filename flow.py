9ff97a46590f90896c034e5852b39708200a72c8
pulkit0795 <pulkit0795@yahoo.com>
2021-09-30 00:30:37 +0530

    Added some more conditins

--- flow.py
+++ flow.py
@@ -69,6 +69,35 @@ def process(l,incomingMsg):
             '*7.* Yak\n' \
             '*8.* Poultry 🐓\n' \
 
+    elif l[1:] == ['3']:
+        r = 'Hey Vet!\nWhat do you want ?\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Want to Register yourself \n' \
+            '*2.* Want Vaccination and Deworming Handbook \n' \
+
+    elif l[1:] == ['4']:
+        r = 'Hey Vet!\nWhat do you want ?\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Want to Register yourself \n' \
+            '*2.* Want Vaccination and Deworming Handbook \n' \
+        
+    elif l[1:] == ['5']:
+        r = 'Hey Buddy!\nWhat do you want ?\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Want Vaccination and Deworming Handbook \n' \
+
+
+    elif l[1:] == ['6']:
+        r = 'Which general service do you want ?\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Animal Insurance \n' \
+            '*2.* Animal Adoption \n' \
+            '*3.* Buy Pet \n' \
+            '*4.* Anti-Animal Welfare Reporting \n' \
+            '*5.* E-Platform for animal farmers \n' \
+            '*6.* Mobile Applications \n' \
+            
+
     # ----------------- Level 1 - End ---------------------
 
     # ----------------- Level 2 - Begin ---------------------
@@ -96,16 +125,124 @@ def process(l,incomingMsg):
             '*5.* Boarding 🏡\n' \
             '*6.* FAQs ❓\n' \
 
+    elif l[1:] == ['1','3']:
+        r = 'What service / info do you want for your Rabbit? 🐰\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* FAQs ❓\n' \
+
+    elif l[1:] == ['1','4']:
+        r = 'What service / info do you want for your Guinea Pig? 🐭\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*3.* Feed 🍲\n' \
+            '*6.* FAQs ❓\n' \
+
+    elif l[1:] == ['2','1']:
+        r = 'What service / info do you want for your Cattle / Buffalo? 🐄\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Foder 🍶\n' \
+            '*5.* Others 🏡\n' \
+            '*6.* Animal Tagging \n' \
+            '*7.* ETT \n' \
+            '*8.* Sex Sorted Semen \n' \
+            '*9.* Semen Stations \n' \
+
+    elif l[1:] == ['2','2']:
+        r = 'What service / info do you want for your Pig? 🐖\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Immunity and Health Boosters 🍶\n' \
+
+    elif l[1:] == ['2','3']:
+        r = 'What service / info do you want for your Horse? 🐎\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Immunity and Health Boosters 🍶\n' \
+
+    elif l[1:] == ['2','4']:
+        r = 'What service / info do you want for your Camel? 🐪\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Immunity and Health Boosters 🍶\n' \
+
+    elif l[1:] == ['2','5']:
+        r = 'What service / info do you want for your Sheep and Goat? 🐑\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Immunity and Health Boosters 🍶\n' \
+
+    elif l[1:] == ['2','6']:
+        r = 'What service / info do you want for your Mithun? 🐃\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Immunity and Health Boosters 🍶\n' \
+
+    elif l[1:] == ['2','7']:
+        r = 'What service / info do you want for your Yak? \n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Immunity and Health Boosters 🍶\n' \
+
+    elif l[1:] == ['2','8']:
+        r = 'What service / info do you want for your Poultry? 🐓\n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* Vaccination 💉\n' \
+            '*2.* Deworming 💊\n' \
+            '*3.* Feed 🍲\n' \
+            '*4.* Immunity and Health Boosters 🍶\n' \
+
+    elif l[1:] == ['6','5']:
+        r = 'What E-Platform do you want to try? \n\n' \
+            'Please choose from the following options: \n\n' \
+            '*1.* E-Gopala \n' \
+            '*2.* Kisan Sarthi \n' \
+            '*3.* Kisan Mitr \n' \
+            '*4.* Farmer Portal of India \n' \
+            '*5.* DAHD \n' \
+
+
     # ----------------- Level 2 - End ---------------------
 
 
     # ----------------- Level 3 - Begin ---------------------
+        # Update their charts location
 
     elif l[1:] == ['1','1','1']:
         r = 'Here\'s the vaccination chart for your dog ! 🎉\n\n' \
             'Nearest Vaccination Centers - *Feature Coming Soon\n*'
         m = 'https://firebasestorage.googleapis.com/v0/b/jeevbot.appspot.com/o/dogvcc.jpg?alt=media&token=b2e7e0d5-092f-436c-af20-ee7f0affd90c'
         mediaFlag = 1
+
+    elif l[1:] == ['1','2','1']:
+        r = 'Here\'s the vaccination chart for your cat ! 🎉\n\n' \
+            'Nearest Vaccination Centers - *Feature Coming Soon\n*'
+        m = 'https://firebasestorage.googleapis.com/v0/b/jeevbot.appspot.com/o/dogvcc.jpg?alt=media&token=b2e7e0d5-092f-436c-af20-ee7f0affd90c'
+        mediaFlag = 1
+
+    elif l[1:] == ['1','3','1']:
+        r = 'Here\'s the vaccination chart for your Rabbit ! 🎉\n\n' \
+            'Nearest Vaccination Centers - *Feature Coming Soon\n*'
+        m = 'https://firebasestorage.googleapis.com/v0/b/jeevbot.appspot.com/o/dogvcc.jpg?alt=media&token=b2e7e0d5-092f-436c-af20-ee7f0affd90c'
+        mediaFlag = 1
+
+    
 
 
 
